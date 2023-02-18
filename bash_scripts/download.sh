06e5b19e541505c82008049e2738ae61883c5ef1
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-02-18 15:40:49 -0500

    add download line

--- bash_scripts/download.sh
+++ bash_scripts/download.sh
@@ -24,6 +24,15 @@ do
             fastq2="${response_dir}${acc}_2.fastq.gz"
             line1=`egrep -o "ftp.sra.ebi.ac.uk/vol1/fastq/SRR150/[0-9]{3}/${acc}/${acc}_1.fastq.gz" $ena_report`
             line2=`egrep -o "ftp.sra.ebi.ac.uk/vol1/fastq/SRR150/[0-9]{3}/${acc}/${acc}_2.fastq.gz" $ena_report`
+            
+            if [ ! -z "$line1" ]
+            then
+                wget -O $fastq1 ftp://$line1
+            fi
+            if [ ! -z "$line2" ]
+            then
+                wget -O $fastq2 ftp://$line2
+            fi
         fi
     fi
 done < $srr_list
