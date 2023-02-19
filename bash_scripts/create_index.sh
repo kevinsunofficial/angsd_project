f5034232a0a6f9bce4ee250af871b15e65f4924b
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-02-19 14:15:06 -0500

    change genome index version to hg38

--- bash_scripts/create_index.sh
+++ bash_scripts/create_index.sh
@@ -10,19 +10,20 @@
 #SBATCH --mail-type=ALL
 
 
-genome_dir="/athena/angsd/scratch/yus4008/project/dataset/hs1_genome/"
-genome_seq="${genome_dir}hs1.fa"
-genome_annot="${genome_dir}hs1.110.20220412.ncbiRefSeq.gtf"
-genome_seq_gz="${genome_dir}hs1.fa.gz"
+dataset_dir="/athena/angsd/scratch/yus4008/project/dataset/"
+genome_dir="${dataset_dir}hg38_genome/"
+genome_seq="${genome_dir}hg38.fa"
+genome_annot="${genome_dir}hg38.ncbiRefSeq.gtf"
+genome_seq_gz="${genome_seq}.gz"
 genome_annot_gz="${genome_annot}.gz"
-ref_dir="/athena/angsd/scratch/yus4008/project/dataset/hs1_STARindex"
+ref_dir="${dataset_dir}hg38_STARindex"
 
 
 if [ ! -f $genome_seq ]
 then
     if [ ! -f $genome_seq_gz ]
     then
-        wget -O $genome_seq_gz "https://hgdownload.soe.ucsc.edu/goldenPath/hs1/bigZips/hs1.fa.gz"
+        wget -O $genome_seq_gz "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
     fi
     gzip -d $genome_seq_gz
 fi
@@ -31,7 +32,7 @@ if [ ! -f $genome_annot ]
 then
     if [ ! -f $genome_annot_gz ]
     then
-        wget -O $genome_annot_gz "https://hgdownload.soe.ucsc.edu/goldenPath/hs1/bigZips/genes/hs1.110.20220412.ncbiRefSeq.gtf.gz"
+        wget -O $genome_annot_gz "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.ncbiRefSeq.gtf.gz"
     fi
     gzip -d $genome_annot_gz
 fi
