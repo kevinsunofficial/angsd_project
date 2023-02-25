43b55049d1d90f0686ddea261c8b6d67bec7d614
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-02-25 08:12:47 -0500

    modify bamqc

--- bash_scripts/bamqc.sh
+++ bash_scripts/bamqc.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=angsd_class
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=bamqc
+#SBATCH --job-name=qc_analysis
 #SBATCH --time=08:00:00
 #SBATCH --mem=32G
 #SBATCH --mail-user=yus4008@med.cornell.edu
@@ -12,10 +12,10 @@
 
 script_dir="/home/yus4008/cmpb5004/project/angsd_project/bash_scripts/"
 dataset_dir="/athena/angsd/scratch/yus4008/project/dataset/"
-aln_dir="/athena/angsd/scratch/yus4008/project/dataset/alignments/"
+aln_dir="/athena/angsd/scratch/yus4008/project/dataset/trim_alignments/"
 qc_dir="${dataset_dir}qc_analysis/"
 bamqc_dir="${qc_dir}bamqc/"
-multiqc_dir="${qc_dir}multiqc/bamqc/"
+multiqc_dir="${qc_dir}multiqc/STAR/"
 
 
 mamba activate angsd
@@ -32,7 +32,7 @@ mamba deactivate
 
 mamba activate multiqc
 
-echo -e "multiqc -o ${multiqc_dir} ${bamqc_dir}"
+echo -e "multiqc -o ${multiqc_dir} ${aln_dir}"
 multiqc -o $multiqc_dir $bamqc_dir
 
 mamba deactivate
