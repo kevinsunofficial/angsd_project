dc132856ce93279acf9fea5b7fc920eb63744b52
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-03-18 16:12:40 -0400

    modify featureCounts command

--- bash_scripts/featureCounts.sh
+++ bash_scripts/featureCounts.sh
@@ -4,18 +4,18 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=feature_counts
-#SBATCH --time=48:00:00
-#SBATCH --mem=64G
+#SBATCH --time=12:00:00
+#SBATCH --mem=32G
 #SBATCH --mail-user=yus4008@med.cornell.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=feature_counts.out
 
 bam_dir="/athena/angsd/scratch/yus4008/project/dataset/trim_alignments/"
 gtf="/athena/angsd/scratch/yus4008/project/dataset/hg38_genome/hg38.ncbiRefSeq.gtf"
-fc_dir="/athena/angsd/scratch/yus4008/project/dataset/featureCounts/"
+fc_dir="/athena/angsd/scratch/yus4008/project/dataset/featureCounts/feature_counts.txt"
 
 mamba activate angsd
 
-featureCounts -a $gtf -t exon -g gene_id -o $fc_dir $bam_dir*/*.bam
+featureCounts -p --countReadPairs -a $gtf -t exon -g gene_id -o $fc_dir $bam_dir*/*.bam
 
 mamba deactivate
